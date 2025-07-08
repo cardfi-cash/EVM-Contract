f9f604046a3cbe5e4bef5e3047cd87f6f0cffffa
wikig <carzygodwu@gmail.com>
2025-07-08 11:58:11 +0000

    update card info

--- contracts/card.sol
+++ contracts/card.sol
@@ -7,19 +7,34 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract CardFi {
     
     mapping(address => uint256[]) public cards;
+    mapping(address => bool) public usd;
     bytes[] public cardsData;
     address public owner;
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can call this function");
         _;
     }
+    address public vault;
+    //Events
+    event deposite(address from , uint256 amount , bytes to);
+    event applyViaETH(address from , bytes to , bytes info);
 
-    address public lpAddress;
-    constructor() {
-        cards[msg.sender].push(2);
+    constructor(address[] memory _usd ,address _vault ) {
+        for(uint i = 0 ; i < _usd.length ; i++)
+        {
+            usd[_usd[i]] = true;
+        }
+        vault = _vault;
     }
     function applyETH() external payable {
         require(msg.value > 0, "Must deposit more than 0 ETH");
+    }
+
+    function deposit(address token,uint256 amount , bytes memory to) public returns(uint256) {
+        require(usd[token],"Token not support");
+        IERC20(token).transferFrom(msg.sender, vault, amount);
+        emit deposite(msg.sender, amount, to);
+        return 0;
     }
 
     function getCard(uint256 amount) public view returns(uint256) {
