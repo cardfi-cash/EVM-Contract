d54c3fab556e7285ff25fb89b87280501bdee092
wikig <carzygodwu@gmail.com>
2025-07-09 12:23:43 +0000

    update card sol

--- contracts/card.sol
+++ contracts/card.sol
@@ -5,10 +5,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CardFi {
-    
-    mapping(address => uint256[]) public cards;
     mapping(address => bool) public usd;
-    bytes[] public cardsData;
+    mapping(address => bytes[]) public cards;
+    mapping(address => bytes) public info;
     address public owner;
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can call this function");
@@ -17,17 +16,32 @@ contract CardFi {
     address public vault;
     //Events
     event deposite(address from , uint256 amount , bytes to);
-    event applyViaETH(address from , bytes to , bytes info);
+    event applyCard(address owner , bytes info , address token , uint256 amount);
 
-    constructor(address[] memory _usd ,address _vault ) {
+    uint256 public applyFeeETH = 0;
+    uint256 public applyFeeUSD = 10;
+    constructor(address[] memory _usd ,address _vault,uint256[] memory applyFees ) {
         for(uint i = 0 ; i < _usd.length ; i++)
         {
             usd[_usd[i]] = true;
         }
         vault = _vault;
+        applyFeeETH = applyFees[0];
+        applyFeeUSD = applyFees[1];
     }
     function applyETH() external payable {
-        require(msg.value > 0, "Must deposit more than 0 ETH");
+        require(msg.value >= applyFeeETH, "Must apply more than 0 ETH");
+        _apply(address(0),msg.value);
+    }
+    function applyUSD(address token) external {
+        require(usd[token],"Token not support");
+        require(IERC20(token).transferFrom(msg.sender,vault,applyFeeUSD),"Transfer failed");
+        _apply(token,applyFeeUSD);
+    }
+
+
+    function _apply(address token , uint256 amount) internal {
+        emit applyCard(msg.sender , info[msg.sender],token,amount);
     }
 
     function deposit(address token,uint256 amount , bytes memory to) public returns(uint256) {
@@ -37,12 +51,8 @@ contract CardFi {
         return 0;
     }
 
-    function getCard(uint256 amount) public view returns(uint256) {
-        return 0;
+    function updateInfo(bytes memory _info) public returns (bool) {
+        info[msg.sender] = _info;
+        return true;
     }
-
-    function getUserInfo(address who) public view returns(uint256) {
-        return 0;
-    }
-
 }
--- test/card.js
+++ test/card.js
@@ -8,11 +8,18 @@ const {
   
     describe("Deployment", function () {
       it("Time to test", async function () {
-        const [owner, addr1 ,addr2,addr3,addr4,addr5,addr6,vault] = await ethers.getSigners();
-        console.log("begian")
-        const Fac = (await ethers.getContractFactory("CardFi"));
-        const fac = await Fac.connect(owner).deploy();
-        console.log("🍺 CardFi Deploy: "+fac.target);  
+        const [owner] = await ethers.getSigners();
+        const usdt = "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9";
+        const usdc = "0xaf88d065e77c8cc2239327c5edb3a432268e5831"
+        const applyFee = 1e16.toString();
+        const applyFeeUSD = 1e8.toString();
+        const CAR = (await ethers.getContractFactory("CardFi"));
+        const car = await CAR.connect(owner).deploy(
+          [usdt,usdc],
+          "0x69661b7a8A81b4F4d9cde379750277a802C20F5c",
+          [applyFee , applyFeeUSD]
+        );
+        console.log("🍺 CardFi Deploy: "+car.target);  
         });
     });
   });
